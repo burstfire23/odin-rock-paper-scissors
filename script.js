6e121c41df38f186c7b624865854735131b5cd57
Isaac Liang <burstfire23@gmail.com>
2026-06-02 15:37:30 -0700

    added the logic and tests with console.log to be able to play one round

--- script.js
+++ script.js
@@ -17,5 +17,43 @@ function getHumanChoice(){
     return prompt("Choose rock, paper, or scissors");
 }
 
+function playRound(humanChoice, computerChoice){
+    humanChoice = humanChoice.toLowerCase();
+    resultString = ""
 
+    if (humanChoice == "paper" && computerChoice == "rock") {
+        resultString = `You win! ${humanChoice} beats ${computerChoice};`
+        humanScore++;
+    } else if (computerChoice == "paper" && humanChoice == "rock") {
+        resultString = `You lose! ${computerChoice} beats ${humanChoice};`
+        computerScore++;
+    } else if (humanChoice == "scissors" && computerChoice == "paper") {
+        resultString = `You win! ${humanChoice} beats ${computerChoice};`
+        humanScore++;
+    } else if (computerChoice == "scissors" && humanChoice == "paper") {
+        resultString = `You lose! ${computerChoice} beats ${humanChoice};`
+        computerScore++;
+    } else if (humanChoice == "rock" && computerChoice == "scissors") {
+        resultString = `You win! ${humanChoice} beats ${computerChoice};`
+        humanScore++;
+    } else if (computerChoice == "rock" && humanChoice == "scissors") {
+        resultString = `You lose! ${computerChoice} beats ${humanChoice};`
+        computerScore++;
+    } else {
+        resultString = "It's a tie!";
+    }
+
+    console.log(`Your choice: ${humanChoice}`);
+    console.log(`Computer choice: ${computerChoice}`);
+
+    console.log(resultString);
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection)
+console.log(`User: ${humanScore}`);
+console.log(`Computer: ${computerScore}`);
 
