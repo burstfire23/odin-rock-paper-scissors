330a07e10c6870044b5cdc702ef08063baabe2fd
Isaac Liang <burstfire23@gmail.com>
2026-06-02 15:46:29 -0700

    added logic to play a game of five rounds with console.log for testing

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-let humanScore = 0;
-let computerScore = 0;
+
 
 function getComputerChoice(){
     random = Math.floor(Math.random() * 3);
@@ -17,43 +16,63 @@ function getHumanChoice(){
     return prompt("Choose rock, paper, or scissors");
 }
 
-function playRound(humanChoice, computerChoice){
-    humanChoice = humanChoice.toLowerCase();
-    resultString = ""
 
-    if (humanChoice == "paper" && computerChoice == "rock") {
-        resultString = `You win! ${humanChoice} beats ${computerChoice};`
-        humanScore++;
-    } else if (computerChoice == "paper" && humanChoice == "rock") {
-        resultString = `You lose! ${computerChoice} beats ${humanChoice};`
-        computerScore++;
-    } else if (humanChoice == "scissors" && computerChoice == "paper") {
-        resultString = `You win! ${humanChoice} beats ${computerChoice};`
-        humanScore++;
-    } else if (computerChoice == "scissors" && humanChoice == "paper") {
-        resultString = `You lose! ${computerChoice} beats ${humanChoice};`
-        computerScore++;
-    } else if (humanChoice == "rock" && computerChoice == "scissors") {
-        resultString = `You win! ${humanChoice} beats ${computerChoice};`
-        humanScore++;
-    } else if (computerChoice == "rock" && humanChoice == "scissors") {
-        resultString = `You lose! ${computerChoice} beats ${humanChoice};`
-        computerScore++;
-    } else {
-        resultString = "It's a tie!";
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    let tie = 0;
+
+    function playRound(humanChoice, computerChoice){
+        humanChoice = humanChoice.toLowerCase();
+        resultString = ""
+
+        if (humanChoice == "paper" && computerChoice == "rock") {
+            resultString = `You win! ${humanChoice} beats ${computerChoice};`
+            humanScore++;
+        } else if (computerChoice == "paper" && humanChoice == "rock") {
+            resultString = `You lose! ${computerChoice} beats ${humanChoice};`
+            computerScore++;
+        } else if (humanChoice == "scissors" && computerChoice == "paper") {
+            resultString = `You win! ${humanChoice} beats ${computerChoice};`
+            humanScore++;
+        } else if (computerChoice == "scissors" && humanChoice == "paper") {
+            resultString = `You lose! ${computerChoice} beats ${humanChoice};`
+            computerScore++;
+        } else if (humanChoice == "rock" && computerChoice == "scissors") {
+            resultString = `You win! ${humanChoice} beats ${computerChoice};`
+            humanScore++;
+        } else if (computerChoice == "rock" && humanChoice == "scissors") {
+            resultString = `You lose! ${computerChoice} beats ${humanChoice};`
+            computerScore++;
+        } else {
+            resultString = "It's a tie!";
+            tie++;
+        }
+
+        console.log(`Your choice: ${humanChoice}`);
+        console.log(`Computer choice: ${computerChoice}`);
+
+        console.log(resultString);
+
     }
 
-    console.log(`Your choice: ${humanChoice}`);
-    console.log(`Computer choice: ${computerChoice}`);
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
+    playRound(getHumanChoice(), getComputerChoice());
 
-    console.log(resultString);
+    if (humanScore > computerScore){
+        console.log(`You win! The score after five rounds was ${humanScore}-${computerScore}-${tie}`);
+    } else if (humanScore < computerScore) {
+        console.log(`You lose! The score after five rounds was ${computerScore}-${humanScore}-${tie}`);
+    } else {
+        console.log("It's a tie!");
+    }
 
 }
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
+playGame();
 
-playRound(humanSelection, computerSelection)
-console.log(`User: ${humanScore}`);
-console.log(`Computer: ${computerScore}`);
 
